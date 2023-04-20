cee3010620c7dd08feaaf6fc3b1d36c43754cfe3
ggam-nyang <v44ads@naver.com>
2023-04-20 20:05:50 +0900

    git init

---
+++ src/main/kotlin/io/ggamnyang/bt/BottleLetterApplication.kt
@@ -0,0 +1,11 @@
+package io.ggamnyang.bt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BottleLetterApplication
+
+fun main(args: Array<String>) {
+	runApplication<BottleLetterApplication>(*args)
+}
---
+++ src/test/kotlin/io/ggamnyang/bt/BottleLetterApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.ggamnyang.bt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BottleLetterApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
